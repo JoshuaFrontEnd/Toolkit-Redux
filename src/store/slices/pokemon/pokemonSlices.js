9b67d849c554b67b45b19030a87fac5e9f862d71
Joshua <hola@joshua.cl>
2023-09-22 16:00:47 -0300

    Configurando Axios

--- src/store/slices/pokemon/pokemonSlices.js
+++ src/store/slices/pokemon/pokemonSlices.js
@@ -15,7 +15,13 @@ export const pokemonSlice = createSlice({
       state.isLoading = true;
     },
     setPokemons: ( state, action ) => {
-      console.log( action );
+      // Como ya cargue los pokemons los dejo de cargar
+      state.isLoading = false;
+
+      // Configuro el "Payload" (action)
+      state.page = action.payload.page;
+      state.pokemons = action.payload.pokemons;
+
     }
   }
 });
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,7 +1,8 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlices";
 
 // Creo mi funcion "Thunk" (action) "getPokemon"
-export const getPokemons = () => {
+export const getPokemons = ( page = 0 ) => {
   // Y esta accion "despachara" uno o varios "reducers" desde los "slices" creados anteriormente, usando como argumento la funcion "dispatch" que es la que "despacha" y "getState" por si necesito obtener el "estado" global de la app
   return async( dispatch, getState ) => {
 
@@ -10,7 +11,15 @@ export const getPokemons = () => {
 
     // TODO: realizar petición http
 
-    // dispatch( setPokemons() );
+    // Usando la petición con "fetch"
+    // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+    // const data = await resp.json();
+
+    // Usando la petición con "Axios"
+    const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`);
+
+    // Envio el "Payload" configurado anteriormente
+    dispatch( setPokemons({ page: page + 1, pokemons: data.results }) );
 
   }
 }
