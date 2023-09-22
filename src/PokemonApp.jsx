392c3ed12312a5a23562778b18b1a7f1eaff3215
Joshua <hola@joshua.cl>
2023-09-22 19:30:55 -0300

    Mostrando los pokemons de manera paginada

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
 
   // Para poder "despachar" una "accion" uso el "Hook" de "React-Redux" "useDispatch"
   const dispatch = useDispatch();
+
+  // Seteo/extraigo, el estado actual (pokemonSlices)
+  const { page, pokemons = [], isLoading } = useSelector( state => state.pokemons );
 
   // Si quiero hacer algo cuando el componente es creado por primera vez y ejecutarlo una sola vez uso el "Hook" "useEffect" de la siguiente manera: en este caso, despachando un "Thunk"
   useEffect(() => {
@@ -17,12 +20,33 @@ export const PokemonApp = () => {
     <>
       <h1>PokemonApp</h1>
       <hr />
+      <span>Loading: { isLoading ? 'true' : 'false' }</span>
 
       <ul>
-        <li>hola</li>
-        <li>hola</li>
-        <li>hola</li>
+        {
+          pokemons.map( ({ name }) => (
+            <li key={ name }>{ name }</li>
+          ))
+          /* --------------------------------------------------------------
+
+            Lo anterior seria igual a esto en javascript vanilla:
+
+              pokemons.map( pokemon => (
+                <li>{ pokemons.name }</li>
+              ))
+
+            lo que sucede es que React en las listas siempre pide un "key", este debe ser unico e irrepetible, por eso es mejor, desestructurar y tomar una propidad del objeto que nos sirva como key, en este caso la propiedad "name" del objeto "pokemon" nos sirve
+
+          ---------------------------------------------------------------- */
+        }
       </ul>
+
+      <button
+        disabled={ isLoading }
+        onClick={ () => dispatch( getPokemons( page ) ) }
+      >
+        Next
+      </button>
     </>
   )
 }
